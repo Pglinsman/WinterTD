c3ca7c946ac6403af9e122321f52121d751f22a5
Pglinsman <pglinsman@gmail.com>
2016-12-21 15:49:24 -0500

    UFO Tutorial

---
+++ UFO Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    public float speed = 10;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = speed * Input.GetAxis("Horizontal");
+        float moveVertical = speed * Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement);
+    }
+}
